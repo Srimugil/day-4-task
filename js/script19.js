b03e1e10fb064479af766d88f044ae75df609eb9
Srimugil <srimugil2001@gmail.com>
2024-05-26 18:32:14 +0530

    first commit

---
+++ js/script19.js
@@ -0,0 +1,22 @@
+function isPalindrome(N)
+    {
+        let str = "" + N;
+        let len = str.length;
+        for (let i = 0; i < parseInt(len / 2, 10); i++)
+        {
+            if (str[i] != str[len - 1 - i ])
+                return false;
+        }
+        return true;
+    }
+    Palindrome = (arr, n) =>
+        {
+        
+            for (let i = 0; i < n; i++)
+            {
+                let ans = isPalindrome(arr[i]);
+                if (ans == false)
+                    return false;
+            }
+            return true;
+        }
